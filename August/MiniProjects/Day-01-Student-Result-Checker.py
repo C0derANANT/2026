5a200a157c0084b381102f3c6d2b31efcfb68d24
Anant Aggarwal <150260673+C0derANANT@users.noreply.github.com>
2026-08-09 15:56:34 +0530

    Day 01: Added Student Result Checker Mini Project
    Built a Student Result Checker using Python fundamentals learned on Day 01. The project takes student details and marks, calculates total marks and percentage, checks subject-wise passing criteria, and assigns grades using comparison operators and conditional statements. This project helped reinforce input handling, calculations, variables, operators, and basic decision-making.

---
+++ August/MiniProjects/Day-01-Student-Result-Checker.py
@@ -0,0 +1,25 @@
+name = input("Enter The Name : ")
+marks1 = int(input("Enter Marks Of First Subject : "))
+marks2 = int(input("Enter Marks Of Second Subject : "))
+marks3 = int(input("Enter Marks Of Third Subject : "))
+
+total = marks1 + marks2 + marks3
+percentage = total / 3
+
+print(f"Percentage : {percentage:.2f}%")
+
+if marks1 >= 33 and marks2 >= 33 and marks3 >= 33:
+
+    if percentage > 85:
+        grade = "A+"
+    elif percentage > 70:
+        grade = "B"
+    elif percentage > 60:
+        grade = "C"
+    else:
+        grade = "D"
+
+    print(f"{name} You Scored Grade {grade}")
+
+else:
+    print(f"{name} You Failed")
