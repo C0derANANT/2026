036fcd5e302bd89926bb80d9ab08e2e19d0cf625
Anant Aggarwal <150260673+C0derANANT@users.noreply.github.com>
2026-08-10 18:38:19 +0530

    working in progress

--- August/Questions/Aug-10/q3.py
+++ August/Questions/Aug-10/q3.py
@@ -1,1 +1,9 @@
 # Email Check
+
+n=input("Enter An E-Mail : ")
+ch_before=n.index('@')-1
+no_of_ch_after=len(n)-1-n.index("@")
+if n.count("@")==1 and ch_before>=0 and no_of_ch_after>0 and n[n.index("@")+1]!=" " : 
+    print("Valid")
+else:
+    print("Invalid")
