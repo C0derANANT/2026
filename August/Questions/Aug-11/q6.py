6f8ed895b0b5ebb7af6b7c24bb0912c080c35ed1
Anant Aggarwal <150260673+C0derANANT@users.noreply.github.com>
2026-08-11 17:24:32 +0530

    Create q6.py

---
+++ August/Questions/Aug-11/q6.py
@@ -0,0 +1,16 @@
+a=int(input("Enter Length Of First Side : "))
+b=int(input("Enter Length Of Second Side : "))
+c=int(input("Enter Length Of Third Side : "))
+if a>0 and b>0 and c>0:
+    if a+b>c and b+c>a and c+a >b:
+        print("Valid Triangle")
+        if a==b and b==c and c==a:
+            print("Equilateral Triangle")
+        elif a!=b and b!=c and c!=a:
+            print("Scalene Triangle")
+        else:
+            print("Isoceles Triangle")
+    else:
+        print("Invalid Triangle")
+else:
+    print("Invalid , Negative Sides")
