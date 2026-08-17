a7dc793121aa7c9d38085caed2102990788f57c0
Anant Aggarwal <150260673+C0derANANT@users.noreply.github.com>
2026-08-17 18:51:57 +0530

    renaming

---
+++ August/DSA/Session-02/index.py
@@ -0,0 +1,30 @@
+# Variables , DataTypes , Operators 
+a=10 # Integer DataType
+b=10.5 # Float DataType
+c="Hello" # String DataType
+d=True # Boolean DataType
+e=None # None DataType
+
+a+=1 # Increment Operator
+b-=1 # Decrement Operator
+
+add=a+b # Addition Operator
+sub=a-b # Subtraction Operator
+mul=a*b # Multiplication Operator
+div=a/b # Division Operator
+mod=a%b # Modulus Operator
+print("Addition:",add)
+print("Subtraction:",sub)
+print("Multiplication:",mul)
+print("Division:",div)
+print("Modulus:",mod)
+# Truth Table
+print(True and True) # True
+print(True and False) # False
+print(False and True) # False
+print(False and False) # False
+
+print(True or True) # True
+print(True or False) # True
+print(False or True) # True 
+print(False or False) # False
