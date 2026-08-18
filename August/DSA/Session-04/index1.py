960eb74dd2a3aad0f22a3a34b6f8cd30540fc73e
Anant Aggarwal <150260673+C0derANANT@users.noreply.github.com>
2026-08-18 19:56:36 +0530

    Update index1.py

--- August/DSA/Session-04/index1.py
+++ August/DSA/Session-04/index1.py
@@ -1,7 +1,21 @@
 # Square Pattern
 n=int(input("ENTER A NUMBER : "))
-pattern=''
-for i in range(n):
-    pattern+=str(i+1)+" "
-pattern+='\n'
-print(pattern*n)
+
+# 1
+# n=int(input("ENTER A NUMBER : "))
+# pattern=''
+# for i in range(n):
+#     pattern+=str(i+1)+" "
+# pattern+='\n'
+# print(pattern*n)
+
+# 2
+# 1 2 3
+# 4 5 6 
+# 7 8 9
+
+n=int(input("Enter A Number : "))
+for i in range(0,n):
+    for j in range(n*i+1,n*(i+1)+1):
+        print(j,end=' ')
+    print()
