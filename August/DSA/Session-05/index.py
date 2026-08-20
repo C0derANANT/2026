8856b361dbc6e6a3a9a0516e041d92b6dfc2d303
Anant Aggarwal <150260673+C0derANANT@users.noreply.github.com>
2026-08-20 14:33:36 +0530

    Post

--- August/DSA/Session-05/index.py
+++ August/DSA/Session-05/index.py
@@ -5,8 +5,19 @@ def square(n):
 def cube(n):
     return n**3
 
-n=int(input("Enter A Number : "))
-print("Square of",n,"is",square(n))
-# print("Cube of",n,"is",cube(n))
+def prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
-lambda_square = lambda n: n**2
+def allprime(n):
+    primes = []
+    for i in range(2, n + 1):
+        if prime(i):
+            primes.append(i)
+    return primes
+n=int(input("Enter A Number : "))
+print("Prime numbers up to",n,"are:",allprime(n))
