f4ec4fa3aa2e82cd280a2319747941cf6004df80
Anant Aggarwal <150260673+C0derANANT@users.noreply.github.com>
2026-08-20 13:16:55 +0530

    Add Python pattern programs
    Adds a new DSA session script with commented diamond pattern examples and an implementation of the butterfly pattern in August/DSA/Session-04/zmix.py.

---
+++ August/DSA/Session-04/zmix.py
@@ -0,0 +1,20 @@
+# # Diamond Patterns 
+# # 15
+# n = int(input("Enter A Number : "))
+# print(" " * n + "*")
+# for i in range(1, n):
+#     print(" " * (n-i) + "*" + " " * (2*i-1) + "*")
+# for i in range(n-2, 0, -1):
+#     print(" " * (n-i) + "*" + " " * (2*i-1) + "*")
+# print(" " * n + "*")
+
+
+
+
+# Butterfly Pattern
+# 16
+n=int(input("Enter A Number : "))
+for i in range(1,n+1):
+    print(" *"*i+"  "*(n*2-2*i)+" *"*i)
+for i in range(n,0,-1):
+    print(" *"*i+"  "*(n*2-2*i)+" *"*i)
