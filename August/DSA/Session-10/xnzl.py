7a204b4bf3992fb3bf3e40bcfaf3d555639192b4
Anant Aggarwal <150260673+C0derANANT@users.noreply.github.com>
2026-08-21 17:34:14 +0530

    Update xnzl.py

--- August/DSA/Session-10/xnzl.py
+++ August/DSA/Session-10/xnzl.py
@@ -1,37 +1,37 @@
-# # l1= [1, 2, 3, 2]
+# l1= [1, 2, 3, 2]
 
-# # for i in l1:
-# #     if l1.count(i)>1:
-# #         print("True")
-# #         break
-# # else:
-# #     print("False")
-
-# # l1 = [4, 2, 7, 1, 8]
-
-# # largest = l1[0]
-# # for i in l1:
-# #     if i > largest:
-# #         largest = i
-
-# # print(largest)
-
+# for i in l1:
+#     if l1.count(i)>1:
+#         print("True")
+#         break
+# else:
+#     print("False")
 
 # l1 = [4, 2, 7, 1, 8]
 
 # largest = l1[0]
-# second = float('-inf')
-# print(second)
-
-# for i in l1[1:]:
+# for i in l1:
 #     if i > largest:
-#         second = largest
 #         largest = i
-#     elif i > second and i != largest:
-#         second = i
 
-# print(second)
 # print(largest)
+
+
+l1 = [4, 2, 7, 1, 8]
+
+largest = l1[0]
+second = float('-inf')
+print(second)
+
+for i in l1[1:]:
+    if i > largest:
+        second = largest
+        largest = i
+    elif i > second and i != largest:
+        second = i
+
+print(second)
+print(largest)
 
 
 # Moving Zeroes To The End
