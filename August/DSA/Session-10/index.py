0a08e3183bd962600341b921879363d587a7799c
Anant Aggarwal <150260673+C0derANANT@users.noreply.github.com>
2026-08-21 13:53:37 +0530

    Add implementation of Kadane's Algorithm and zeroes moving logic

---
+++ August/DSA/Session-10/index.py
@@ -0,0 +1,22 @@
+# Kadane's Algorithm
+# ALL the subarrays
+
+# l1 = [1, 2, 3, 4, 5]
+l1=[3,-4,5,4,-1,7,-8]
+all_arrays = []
+all_sum = []
+
+for i in range(len(l1)):
+    for j in range(i, len(l1)):
+        print(l1[i:j+1],end=' ')
+        all_arrays.append(l1[i:j+1])
+    print()
+
+# print(all_arrays)
+
+for arr in all_arrays:
+    all_sum.append(sum(arr))
+
+# print(all_sum)
+print(f"\n\nThe Substring With Max Sum Is  : {all_arrays[all_sum.index(max(all_sum))]}\n\n")
+
---
+++ August/DSA/Session-10/xnzl.py
@@ -0,0 +1,43 @@
+# # l1= [1, 2, 3, 2]
+
+# # for i in l1:
+# #     if l1.count(i)>1:
+# #         print("True")
+# #         break
+# # else:
+# #     print("False")
+
+# # l1 = [4, 2, 7, 1, 8]
+
+# # largest = l1[0]
+# # for i in l1:
+# #     if i > largest:
+# #         largest = i
+
+# # print(largest)
+
+
+# l1 = [4, 2, 7, 1, 8]
+
+# largest = l1[0]
+# second = float('-inf')
+# print(second)
+
+# for i in l1[1:]:
+#     if i > largest:
+#         second = largest
+#         largest = i
+#     elif i > second and i != largest:
+#         second = i
+
+# print(second)
+# print(largest)
+
+
+# Moving Zeroes To The End
+l1=[0, 1, 0, 3, 12]
+# print(l1.count(0))
+for i in range(l1.count(0)):
+    l1.remove(0)
+    l1.append(0)
+print(l1)   
