0e657e550d06ebb363d9c4750ac8bfbe9545a29c
Anant Aggarwal <150260673+C0derANANT@users.noreply.github.com>
2026-08-21 21:33:47 +0530

    Create index.py

---
+++ August/DSA/Apna College/Session-01/index.py
@@ -0,0 +1,21 @@
+# Pseudo Code
+
+# Area Of A Square
+n=int(input("Enter the length of the side of the square: "))
+print("Area of the square is: ", n**2)
+
+
+# Minimum of two numbers
+n1=int(input("Enter the first number: "))
+n2=int(input("Enter the second number: "))
+if n1<n2:
+    print("Minimum number is: ", n1)
+else:
+    print("Minimum number is: ", n2)
+
+# Is number even or odd
+n=int(input("Enter a number: "))
+if n%2==0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
