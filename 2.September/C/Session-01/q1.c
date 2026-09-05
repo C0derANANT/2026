2928377703d8de0d1cbe46b6cf0a206c8cd4eedc
Anant Aggarwal <150260673+C0derANANT@users.noreply.github.com>
2026-09-05 23:23:45 +0530

    Update q1.c

--- 2.September/C/Session-01/q1.c
+++ 2.September/C/Session-01/q1.c
@@ -7,7 +7,7 @@ int main(){
     printf("Perimeter: %d units\n",side*4);           // Perimeter OF A Square
     return 0;
 }
-
+ 
 // --------------x--------------x--------------x--------------x--------------x------
 
 // #include<stdio.h>
@@ -16,6 +16,6 @@ int main(){
 //     printf("Enter Radius Of A Circle: ");
 //     scanf("%f",&radius);
 //     printf("Area: %f sq.units\n",3.14*radius*radius);     // AREA OF A Circle
-//     printf("Circumference: %f units\n",2*3.14*radius); // Circumference OF A Circle
+//     printf("Circumference: %f units\n",2 *3.14*radius); // Circumference OF A Circle
 //     return 0;
 // }
