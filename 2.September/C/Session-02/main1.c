fa93a1bdf513ee816074a89b2c365c520dbe6b74
Anant Aggarwal <150260673+C0derANANT@users.noreply.github.com>
2026-09-08 07:17:42 +0530

    Update main1.c

--- 2.September/C/Session-02/main1.c
+++ 2.September/C/Session-02/main1.c
@@ -1,4 +1,11 @@
-#include <stdio.h>j 
+#include <stdio.h>
 // XOR between same bits = 0
 // XOR between different bits = 1
- 
+// Variable Declaration
+int main() {
+    int OldAge = 20;
+    int YearsPassed = 5;
+    int NewAge = OldAge + YearsPassed;
+    printf("New Age: %d\n", NewAge);
+    return 0;
+}
